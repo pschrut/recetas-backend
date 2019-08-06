23fd99e8eb33479178e5834825c28a7f2c02ab06
Pablo <pablo_aaaj@hotmail.com>
2019-08-06 12:04:57 +0200

    wrong url for mongo

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
+process.env.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/recipies';
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
